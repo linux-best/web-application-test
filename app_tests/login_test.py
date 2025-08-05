28e5a3c5e2ecb164ef43a2a8f5596fdbeacaa092
jenkins <amirmahdifhp@gmail.com>
2025-08-05 10:19:52 -0400

    Update ,app_tests/login_test.py

--- app_tests/login_test.py
+++ app_tests/login_test.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get('https://127.0.0.1:3901')
+username_input = driver.find_element(By.ID ,"username")
+password_input = driver.find_element(By.ID ,"password")
+
+username_input.send_keys("admin")
+password_input.send_keys("admin123")
+
+login_button = driver.find_element(By.ID ,"login_button")
+
+login_button.click()
+time.sleep(5)
+
+welcome_text = driver.find_element(By.ID ,"welcome_massege").text
+
+if "welcome, admin" in welcome_text :
+    print("login passed !")
+else:
+    print("login failed !!")
+
+driver.quit()
+
