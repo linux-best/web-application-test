471024c3560abd3d803f4968e95e87ebd63d9fcf
jenkins <amirmahdifhp@gmail.com>
2025-08-06 08:18:08 -0400

    Update ,login_test.py

--- app_tests/login_test.py
+++ app_tests/login_test.py
@@ -7,12 +7,6 @@ import time
 
 chrome_options = Options()
 
-# Add any desired Chrome options, e.g., headless mode for Jenkins
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox") # Important for Jenkins
-chrome_options.add_argument("--disable-dev-shm-usage") # Important for Jenkins
-chrome_options.binary_location = "/usr/bin/google-chrome-stable"
-
 # Replace with the actual path to your chromedriver executable
 service_driver = Service("/usr/local/bin/chromedriver-linux64/chromedriver")
 driver = webdriver.Chrome(service=service_driver , options=chrome_options)
@@ -30,10 +24,10 @@ login_button = driver.find_element(By.NAME ,"Login")
 login_button.click()
 time.sleep(5)
 
-welcome_text = driver.find_element(By.ID ,"welcome_massege").text
+welcome_text = driver.find_element(By.ID ,"main_body").text
 
 #assert "welcome, admin" in welcome_text 
-if "welcome, admin" in welcome_text :
+if "Welcome to Damn Vulnerable Web Application!" in welcome_text :
     print("login passed !")
 else:
     print("login failed !!")
