452b31d891100de418c866e5f1d6c7059fadeb5a
jenkins <amirmahdifhp@gmail.com>
2025-08-09 08:27:32 -0400

    Update ,brute_force_test.py

---
+++ app_tests/brute_force_test.py
@@ -0,0 +1,108 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+User = "username"
+passwd = "password"
+Condition = "You have logged in as 'admin'" 
+
+chrome_options = Options()
+
+# Add any desired Chrome options, e.g., headless mode for Jenkins
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox") # Important for Jenkins
+chrome_options.add_argument("--disable-dev-shm-usage") # Important for Jenkins
+chrome_options.binary_location = "/usr/bin/google-chrome-stable"
+
+# Replace with the actual path to your chromedriver executable
+service_driver = Service("/usr/local/bin/chromedriver-linux64/chromedriver")
+driver = webdriver.Chrome(service=service_driver , options=chrome_options)
+
+def driver_quit():
+    driver.quit()
+
+def Bute_Force():
+    global User
+    global passwd
+    WebDriverWait(driver,5).until(
+        EC.presence_of_all_elements_located((By.XPATH ,"//*[contains(text(),'Brute Force')]"))
+    )
+    brute_field = driver.find_element(By.XPATH ,"//*[contains(text(),'Brute Force')]")
+    brute_field.click()
+
+    WebDriverWait(driver,5).until(
+        EC.presence_of_all_elements_located((By.NAME ,User))
+    )
+    username_input = driver.find_element(By.NAME ,User)
+    WebDriverWait(driver,5).until(
+        EC.presence_of_all_elements_located((By.NAME ,passwd))
+    )
+    password_input = driver.find_element(By.NAME ,passwd)
+
+    username_input.send_keys("admin")
+    password_input.send_keys("password")
+
+    WebDriverWait(driver,5).until(
+        EC.presence_of_all_elements_located((By.NAME ,"Login"))
+    )
+    login_button = driver.find_element(By.NAME ,"Login")
+    login_button.click()
+
+    WebDriverWait(driver,5).until(
+        EC.presence_of_all_elements_located((By.ID ,"main_body"))
+    )    
+    welcome_text = driver.find_element(By.ID ,"main_body").text
+
+    if "Welcome to Damn Vulnerable Web Application!" in welcome_text :
+        print("you have goten access !")
+    else:
+        print("you have failed to get access !!")
+
+def login_test():
+    assert User == "username"
+    assert passwd == "password"
+
+    driver.get('http://127.0.0.1:8081/login.php')
+    print(driver.title)
+    WebDriverWait(driver,5).until(
+        EC.presence_of_all_elements_located((By.NAME ,User))
+    )
+    username_input = driver.find_element(By.NAME ,User)
+    WebDriverWait(driver,5).until(
+        EC.presence_of_all_elements_located((By.NAME ,passwd))
+    )
+    password_input = driver.find_element(By.NAME ,passwd)
+
+    username_input.send_keys("admin")
+    password_input.send_keys("password")
+
+    WebDriverWait(driver,5).until(
+        EC.presence_of_all_elements_located((By.NAME ,"Login"))
+    )
+    login_button = driver.find_element(By.NAME ,"Login")
+
+    login_button.click()
+    time.sleep(5)
+
+    WebDriverWait(driver,5).until(
+        EC.presence_of_all_elements_located((By.ID ,"main_body"))
+    )    
+    welcome_text = driver.find_element(By.ID ,"main_body").text
+    print(welcome_text)
+
+    assert "Welcome to Damn Vulnerable Web Application!" in welcome_text
+    assert Condition in welcome_text
+    
+    if "Welcome to Damn Vulnerable Web Application!" in welcome_text :
+        print("login passed !")
+    else:
+        print("login failed !!")
+    
+login_test()
+Bute_Force()
+driver_quit()
