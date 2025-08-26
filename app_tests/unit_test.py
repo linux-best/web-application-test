84f340bd2dc59efc86617b83c0650e4c97fbaf46
jenkins <amirmahdifhp@gmail.com>
2025-08-26 11:46:04 -0400

    Update ,jenkinsfile

--- app_tests/unit_test.py
+++ app_tests/unit_test.py
@@ -2,13 +2,13 @@ import pytest
 from tests import *
 
 def test_login():
-    assert login_test_section(login_user="admin",login_passwd="123") == "login passed !"
+    assert login_test_section(login_user="admin",login_passwd="password") == "login passed !"
 
 def test_CSRF():
-    assert CSRF_Test_section(new_paassword="password") == "Password Changed !"
+    assert CSRF_Test_section(new_paassword="123") == "Password Changed !"
 
 def test_Brute_Force():
-    assert Brute_Force_Test_section(username="admin",password="password") == "Welcome to the password protected area admin !"
+    assert Brute_Force_Test_section(username="admin",password="123") == "Welcome to the password protected area admin !"
 
 def test_Driver_quit():
     assert driver_quit() == "Done !"
