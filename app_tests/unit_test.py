172c00d61768c444c86023dcbb771beca2424c35
linux-best <amirmahdifhp@gmail.com>
2025-08-28 11:03:08 +0330

    Update unit_test.py
    Signed-off-by: linux-best <amirmahdifhp@gmail.com>

--- app_tests/unit_test.py
+++ app_tests/unit_test.py
@@ -4,13 +4,13 @@ from tests import *
 # NOTE : Security_level = Impossible
 
 def test_login():
-    assert login_test_section(login_user="admin",login_passwd="password") == "login passed !"
+    assert login_test_section(login_user="admin",login_passwd="123") == "login passed !"
 
 def test_CSRF():
-    assert CSRF_Test_section(old_password="password",new_paassword="123") == "Password Changed !"
+    assert CSRF_Test_section(old_password="123",new_paassword="password") == "Password Changed !"
 
 def test_Brute_Force():
-    assert Brute_Force_Test_section(username="admin",password="123") == "Welcome to the password protected area admin !"
+    assert Brute_Force_Test_section(username="admin",password="password") == "Welcome to the password protected area admin !"
 
 def test_Driver_quit():
     assert driver_quit() == "Done !"
