5565b0ff83a1e6c0f8efadbef1f4a7fabd36dab4
linux-best <amirmahdifhp@gmail.com>
2025-08-28 12:57:43 +0330

    Update tests.py
    Signed-off-by: linux-best <amirmahdifhp@gmail.com>

--- app_tests/tests.py
+++ app_tests/tests.py
@@ -74,10 +74,11 @@ def select_level(level_required):
     WebDriverWait(driver,5).until(
         EC.presence_of_all_elements_located((By.CLASS_NAME ,"message"))
     )    
-    level_msg = driver.find_element(By.CLASS_NAME ,"message").text
+    level_msg = driver.find_element(By.ID ,"message").text
+    print(level_msg)
 
     if level_required in level_msg:
-        print(level_msg)
+        #print(level_msg)
         return "level successfully changed !"
     else:
         return "level failed to change !!"
