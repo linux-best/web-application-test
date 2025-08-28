0481d5b5e07d84be8457557197163f45fedeffda
linux-best <amirmahdifhp@gmail.com>
2025-08-28 12:46:08 +0330

    Update unit_test.py
    Signed-off-by: linux-best <amirmahdifhp@gmail.com>

--- app_tests/unit_test.py
+++ app_tests/unit_test.py
@@ -7,7 +7,7 @@ def test_login():
     assert login_test_section(login_user="admin",login_passwd="123") == "login passed !"
 
 def test_level():
-    assert select_level(level_required="Impossible") # levels => Low , Medium , High , Impossible
+    assert select_level(level_required="Impossible") == "level successfully changed !" # levels => Low , Medium , High , Impossible
 
 def test_CSRF():
     #assert CSRF_Test_section(old_password="123",new_paassword="password") == "Password Changed !"
