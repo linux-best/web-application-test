c93d71a72d815202ea85ab6203d933a1bc3f8a7e
jenkins <amirmahdifhp@gmail.com>
2025-08-28 06:40:04 -0400

    Update ,app_tests/unit_test.py

--- app_tests/unit_test.py
+++ app_tests/unit_test.py
@@ -4,16 +4,16 @@ from  tests import *
 # NOTE : Security_level = Impossible
 
 def test_login():
-    assert login_test_section(login_user="admin",login_passwd="123") == "login passed !"
+    assert login_test_section(login_user="admin",login_passwd="password") == "login passed !"
 
 def test_level():
     assert select_level(level_required="Impossible") == "level successfully changed !" # levels => Low , Medium , High , Impossible
 
 def test_CSRF():
-    assert CSRF_Test_section(old_password="123",new_paassword="password") == "Password Changed !"
+    assert CSRF_Test_section(old_password="password",new_paassword="123") == "Password Changed !"
     
 def test_Brute_Force():
-    assert Brute_Force_Test_section(username="admin",password="password") == "Welcome to the password protected area admin !"
+    assert Brute_Force_Test_section(username="admin",password="123") == "Welcome to the password protected area admin !"
     
 def test_Driver_quit():
     assert driver_quit() == "Done !"
